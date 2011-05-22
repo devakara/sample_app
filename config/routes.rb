4db91287d9548d3a24b1f104e1b00de73ba19c5a
Devakara R. Devange <d.devakara@gmail.com>
2011-05-22 23:28:19 +0530

    Finished layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,23 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+  get "users/new"
 
-  get "pages/contact"
+	match '/contact', :to => 'pages#contact'
+	match '/about',   :to => 'pages#about'
+	match '/help',    :to => 'pages#help'
+	# Below statement is more hard wired seemingly
+	match '/home',			:to => 'pages#home'	
+	# We could simply use
+	root :to => 'pages#home'
+	
+	match '/signup', :to => 'users#new'
+
+  #get "pages/home"
+
+  #get "pages/contact"
   
-  get "pages/about"
+  #get "pages/about"
   
-  get "pages/help"
+  #get "pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
